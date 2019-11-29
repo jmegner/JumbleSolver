8b93330bd26f338ad5637849c293265994b57d10
Jacob Egner <jmegner@gmail.com>
2019-11-29 11:12:51 -0600

    setup rust skeleton in VS code and some jumble logic

---
+++ rust/src/main.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::collections::{BTreeMap, BTreeSet};
+use std::vec;
+
+fn main() {
+    let path = Path::new("../greetings.txt");
+    let mut file = File::open(&path).expect("could not open dict file");
+    let mut lineItr = io::BufReader::new(file).lines();
+    let mut sortedToOrigs : BTreeMap<String,BTreeSet<String>> = Default::default();
+    let x : Vec<u8> = Default::default();
+
+    for line in lineItr {
+        let origWord = line.expect("could not read line");
+        let mut chars : Vec<char> = origWord.chars().collect();
+        chars.sort();
+        let sortedWord = String::from_iter(chars);
+        println!("orig={}", origWord);
+        println!("sorted={}", sortedWord);
+        //if sortedToOrigs.contains_key(&word)
+    }
+
+}
+
