d025bdfef14ae2e0974bfee8c41e6e4485767964
Jacob Egner <jmegner@gmail.com>
2019-11-29 12:59:22 -0600

    rust, some improvements with help of Jimmy, clippy, and rustfmt

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,27 +1,32 @@
+use std::collections::{BTreeSet, HashMap};
 use std::error::Error;
 use std::fs::File;
-use std::io::prelude::*;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::{BTreeMap, BTreeSet};
 use std::vec;
 
-fn main() {
-    let path = Path::new("../greetings.txt");
-    let mut file = File::open(&path).expect("could not open dict file");
-    let mut lineItr = io::BufReader::new(file).lines();
-    let mut sortedToOrigs : BTreeMap<String,BTreeSet<String>> = Default::default();
-    let x : Vec<u8> = Default::default();
+/*
+LeadingUpperCamelCase: struct, trait
+snake_case: everything else
 
-    for line in lineItr {
-        let origWord = line.expect("could not read line");
-        let mut chars : Vec<char> = origWord.chars().collect();
+TODO: read "rust turbofish" google results
+*/
+
+// TODO: reference to question operator and function returning a Result
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = Path::new("../greetings.txt");
+    let file = File::open(&path)?;
+    let mut sorted_to_origs: HashMap<String, BTreeSet<String>> = Default::default();
+    let x: Vec<u8> = Default::default();
+
+    for line in io::BufReader::new(file).lines() {
+        let orig_word = line?;
+        let mut chars: Vec<char> = orig_word.chars().collect::<Vec<char>>();
         chars.sort();
-        let sortedWord : String = chars.into_iter().collect();
-        println!("orig={}", origWord);
-        println!("sorted={}", sortedWord);
+        let sorted_word: String = chars.into_iter().collect();
+        println!("orig={}, sorted={}", orig_word, sorted_word);
         //if sortedToOrigs.contains_key(&word)
     }
 
+    Ok(())
 }
-
